bd1a8eabab3ce5d4a1dc62a06970d67610de59ff
MajorMilk <captainjay111@gmail.com>
2024-05-28 18:11:29 -0400

    Updated Dependencies
    Also updated the done control slightly

--- YoutubeToMpx/obj/Release/net7.0-windows/Controls/DownloadControl.g.i.cs
+++ YoutubeToMpx/obj/Release/net7.0-windows/Controls/DownloadControl.g.i.cs
@@ -47,13 +47,13 @@ namespace YoutubeDownloader.Controls {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/YoutubeToMpx;component/controls/downloadcontrol.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/YoutubeToMpx;V1.0.0.0;component/controls/downloadcontrol.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Controls\DownloadControl.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace YoutubeDownloader.Controls {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
--- YoutubeToMpx/obj/Release/net7.0-windows/Controls/URLControl.g.i.cs
+++ YoutubeToMpx/obj/Release/net7.0-windows/Controls/URLControl.g.i.cs
@@ -55,13 +55,13 @@ namespace YoutubeDownloader.Controls {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/YoutubeToMpx;component/controls/urlcontrol.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/YoutubeToMpx;V1.0.0.0;component/controls/urlcontrol.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Controls\URLControl.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -71,7 +71,7 @@ namespace YoutubeDownloader.Controls {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
